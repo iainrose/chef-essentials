8c1f09d80d700c5eb00be4fb754ec5aa120a50a2
ChefDK User <chef@ip-172-31-10-10.ec2.internal>
2017-04-19 23:40:58 +0000

    Release v2.0 of Apache cookbook

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -9,7 +9,10 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+    IPADDRESS: #{node['ipaddress']}
+    HOSTNAME: #{node['hostname']}
+"
 end
 
 service 'httpd' do
