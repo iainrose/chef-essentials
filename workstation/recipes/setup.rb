b91888e8cd9ee2213a810020e338e91a44193f9b
ChefDK User <chef@ip-172-31-10-10.ec2.internal>
2017-04-19 23:29:16 +0000

    Release v2.0 of workstation cookbook

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -1,5 +1,14 @@
 package 'tree'
 
 file '/etc/motd' do
-  content 'Property of Iain Rose'
+  content "Property of Iain Rose
+
+  IPADDRESS: #{node['ipaddress']}
+  HOSTNAME: #{node['hostname']}
+  MEMORY: #{node['memory']['total']}
+  CPU: #{node['cpu']['0']['mhz']}
+"
+ mode '0644'
+ owner 'root'
+ group 'root'
 end
